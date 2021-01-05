05d488913922e657a590832d1034ec21ec4df55f
andresnino1 <andresnino@gmail.com>
2021-01-05 22:30:57 +1100

    implementing widget tweaks in forms

--- fragrances/filters.py
+++ fragrances/filters.py
@@ -4,23 +4,23 @@ from .models import *
 
 
 class FragranceFilter(django_filters.FilterSet):
-    fragrance_code = django_filters.CharFilter(field_name="fragrance_code", lookup_expr='icontains', label='codigo',
-                                               widget=forms.TextInput(
-                                                   attrs={'class': 'form-control', 'placeholder': 'code',
-                                                          'lable': 'code'}))
-    commecial_name = django_filters.CharFilter(field_name="commercial_name", lookup_expr='icontains',
-                                               label='commercial name',
-                                               widget=forms.TextInput(
-                                                   attrs={'class': 'form-control', 'placeholder': 'commercial name'}))
+    # fragrance_code = django_filters.CharFilter(field_name="fragrance_code", lookup_expr='icontains', label='codigo',
+    #                                            widget=forms.TextInput(
+    #                                                attrs={'class': 'form-control', 'placeholder': 'code',
+    #                                                       'lable': 'code'}))
+    # commecial_name = django_filters.CharFilter(field_name="commercial_name", lookup_expr='icontains',
+    #                                            label='commercial name',
+    #                                            widget=forms.TextInput(
+    #                                                attrs={'class': 'form-control', 'placeholder': 'commercial name'}))
 
     class Meta:
         model = FragrancesModel
-        fields = ('fragrance_code',)
+        fields = ('fragrance_code', 'commercial_name')
 
-        # fields = {
-        #     'fragrance_code': ['icontains'],
-        #     'commercial_name': ['icontains'],
-        # }
+        fields = {
+            'fragrance_code': ['icontains'],
+            'commercial_name': ['icontains'],
+        }
 
         # widgets = {
         #     'fragrance_code': forms.TextInput(attrs={'class': 'input', 'placeholder': 'fragrance code'}),
